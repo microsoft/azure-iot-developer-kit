76ab1c3264c0d51263239cb4a4a38925bd24360a
Tanyi Chen <t-tachen@microsoft.com>
2017-06-30 16:59:45 +0800

    make side bar frozen at the right and delete the previos, next button at the bottom of the page

--- docs/assets/js/extended/extended_main.js
+++ docs/assets/js/extended/extended_main.js
@@ -1,3 +1,29 @@
+function sidebarFix(){
+    if (window.innerWidth >= 1024){
+        var position = $('.page__content').position();
+        var articleRight = position.left + $('.page__content').width();
+        
+        if (window.innerWidth < 1280){
+            var sidebarWid = document.body.clientWidth - articleRight - 30;
+            $('.sidebar__right_fix').css('width', sidebarWid);
+        } else {
+            $('.sidebar__right_fix').removeAttr("style")
+        }
+
+        $('.sidebar__right_fix').css('left', articleRight);
+        var articleTop = position.top;
+        var menuHeight = $('.masthead').height();
+        if (articleTop < $(this).scrollTop() + menuHeight){
+            $('.sidebar__right_fix').css('top', menuHeight + 30);
+        } else {
+            $('.sidebar__right_fix').css('top', articleTop - $(this).scrollTop() + 30);
+        }
+    }
+    else {
+        $('.sidebar__right_fix').removeAttr("style")
+    }
+}
+
 function menuScroll(){
     var lastScrollTop = 0
     $(window).bind('scroll', function(){
@@ -14,9 +40,14 @@ function menuScroll(){
             $('.masthead__inner-wrap').removeClass('masthead_shrink');
         }
         lastScrollTop = scrollTop;
+        sidebarFix();
     });
 }
 
 $(document).ready(function(){
     menuScroll();
+    
+    $('.sidebar__right').addClass('sidebar__right_fix');
+    sidebarFix();
+    $(window).resize(sidebarFix);
 });
