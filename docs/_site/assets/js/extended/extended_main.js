3fc50fa320c61f3761e5d71fcc1cc1fb809fd427
Liya Du <liydu@microsoft.com>
2017-07-03 15:51:24 +0800

    Resize the sidebar

---
+++ docs/_site/assets/js/extended/extended_main.js
@@ -0,0 +1,53 @@
+function sidebarFix(){
+    if (window.innerWidth >= 1024){
+        var position = $('.page__content').position();
+        var articleRight = position.left + $('.page__content').width();
+        
+        if (window.innerWidth < 1280){
+            var sidebarWid = document.body.clientWidth - articleRight - 30;
+            $('.sidebar__right_fix').css('width', sidebarWid);
+        } else {
+            $('.sidebar__right_fix').removeAttr("style")
+        }
+        
+        $('.sidebar__right_fix').css('left', articleRight);
+        var articleTop = position.top;
+        var menuHeight = $('.masthead').height();
+        if (articleTop < $(this).scrollTop() + menuHeight){
+            $('.sidebar__right_fix').css('top', menuHeight + 30);
+        } else {
+            $('.sidebar__right_fix').css('top', articleTop - $(this).scrollTop() + 30);
+        }
+    }
+    else {
+        $('.sidebar__right_fix').removeAttr("style")
+    }
+}
+
+function menuScroll(){
+    var lastScrollTop = 0
+    $(window).bind('scroll', function(){
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 50){
+            if (scrollTop > lastScrollTop){
+                $('.masthead').removeClass('masthead_fixed');
+            } else {
+                $('.masthead').addClass('masthead_fixed');
+                $('.masthead__inner-wrap').addClass('masthead_shrink');
+            }
+        } else {
+            $('.masthead').removeClass('masthead_fixed');
+            $('.masthead__inner-wrap').removeClass('masthead_shrink');
+        }
+        lastScrollTop = scrollTop;
+        sidebarFix();
+    });
+}
+
+$(window).load(function(){
+    menuScroll();
+    
+    $('.sidebar__right').addClass('sidebar__right_fix');
+    sidebarFix();
+    $(window).resize(sidebarFix);
+});
